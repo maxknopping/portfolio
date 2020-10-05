8fee6181d6dac1b56806cd9cb728637ea941f57a
maxknopping <mknopping@gmail.com>
2020-10-04 21:09:09 -0400

    Updates

---
+++ precache-manifest.3518f9c4e63a49bc23f96be0ebfe0d65.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49e0196aa1988946125bb5d11268862",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80ee70e4f152bcadedfa",
+    "url": "/portfolio/static/css/2.22fc716b.chunk.css"
+  },
+  {
+    "revision": "efe9dfd1c80bcf02929b",
+    "url": "/portfolio/static/css/main.e0b25300.chunk.css"
+  },
+  {
+    "revision": "80ee70e4f152bcadedfa",
+    "url": "/portfolio/static/js/2.508bf049.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.508bf049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe9dfd1c80bcf02929b",
+    "url": "/portfolio/static/js/main.e63acba8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "27df51de478d80447de0e9dfed4cff58",
+    "url": "/portfolio/static/media/Back.27df51de.jpg"
+  },
+  {
+    "revision": "54fa5c3387ffb073bb389abe13e65559",
+    "url": "/portfolio/static/media/Back.54fa5c33.jpg"
+  },
+  {
+    "revision": "60f9dca90aee5cdd9e7599673724e7d3",
+    "url": "/portfolio/static/media/Back.60f9dca9.jpg"
+  },
+  {
+    "revision": "89438e4dc4bc88df61b286e270936d72",
+    "url": "/portfolio/static/media/Back.89438e4d.jpg"
+  },
+  {
+    "revision": "e2066bd3fda630f3301681954500c5d0",
+    "url": "/portfolio/static/media/Back.e2066bd3.jpg"
+  },
+  {
+    "revision": "2b3c6e74110c2ada5ebbcac9b214b606",
+    "url": "/portfolio/static/media/Front.2b3c6e74.jpg"
+  },
+  {
+    "revision": "3c6faa52c34a0a48f450fbd34da354e7",
+    "url": "/portfolio/static/media/Front.3c6faa52.jpg"
+  },
+  {
+    "revision": "59c0f7e0b8c206a8412e91345a28a807",
+    "url": "/portfolio/static/media/Front.59c0f7e0.jpg"
+  },
+  {
+    "revision": "9878d9dd12fad30eebf1e5e767a0e912",
+    "url": "/portfolio/static/media/Front.9878d9dd.jpg"
+  },
+  {
+    "revision": "a6f31d3a8d0dbd020572a444cb9961c7",
+    "url": "/portfolio/static/media/Front.a6f31d3a.jpg"
+  },
+  {
+    "revision": "fec7c95c75e469789e8304e0905b0b5f",
+    "url": "/portfolio/static/media/KnoppingResume.fec7c95c.pdf"
+  },
+  {
+    "revision": "87cd0726d7126a78c8f541270093b71d",
+    "url": "/portfolio/static/media/StockReturns.87cd0726.png"
+  },
+  {
+    "revision": "e64ab6eaefab0668c8e618aad7a20a1d",
+    "url": "/portfolio/static/media/cards.e64ab6ea.jpg"
+  },
+  {
+    "revision": "2a604b8c03d075e30c4aa7eee431db0c",
+    "url": "/portfolio/static/media/meme_club_logo2.2a604b8c.png"
+  }
+]);

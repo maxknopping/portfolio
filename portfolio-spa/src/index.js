7bd56ca143c6e3b21a706bc9ee906171c1f25050
maxknopping <mknopping@gmail.com>
2020-10-05 11:58:50 -0400

    final commit

--- portfolio-spa/src/index.js
+++ portfolio-spa/src/index.js
@@ -12,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <BrowserRouter basename="/portfolio">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
